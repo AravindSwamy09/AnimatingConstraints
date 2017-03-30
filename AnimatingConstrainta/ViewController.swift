0acc1fdd8d5f817ccd7485d723a233908adb52da
ESS Mac Pro <ESSMacPro@esss-macbook-pro.local>
2017-03-30 06:26:07 -0400

    Added ConstraInts

--- AnimatingConstrainta/ViewController.swift
+++ AnimatingConstrainta/ViewController.swift
@@ -13,9 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet var leftConstraint: NSLayoutConstraint!
     @IBOutlet var rightConstraint: NSLayoutConstraint!
     
+    @IBOutlet var leftDoor: UIView!
+    @IBOutlet var rightDoor: UIView!
+    
+    var doorOpen = false
+    
+    func triggerDoor() {
+        self.view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 3, animations: {
+            
+            self.leftConstraint.constant = self.doorOpen ? -16 : -self.leftDoor.frame.size.width
+            self.rightConstraint.constant = self.doorOpen ? -16 : -self.rightDoor.frame.size.width
+            self.view.layoutIfNeeded()
+            
+        }) { (success) in
+            
+            self.doorOpen = self.doorOpen ? false:true
+            self.triggerDoor()
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        triggerDoor()
+        
     }
 
     override func didReceiveMemoryWarning() {
